1d10b342fb0f357807779e4253dee1fb9bd2e1d5
hamlucas123 <Hamish.Lucas1@student.lshtm.ac.uk>
2022-01-19 14:44:04 +0000

    Integrating Ronnie's code from country_predict.R

--- worldbankCLEAN.R
+++ worldbankCLEAN.R
@@ -1,3 +1,10 @@
 # World Bank Data Cleaning
 
 df <- read.csv('data/country-level-predictors.csv')
+
+# replacing all ".." with NA
+# df[df==".."] <- NA
+
+# create column with last known value for each predictor
+df$lastval <- df[cbind( 1:nrow(df), max.col(!is.na(df),"last") )]
+df$lastval <- as.numeric(df$lastval)

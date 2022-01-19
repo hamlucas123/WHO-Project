22390de962f6b538a2cf809905d16f9aa328b515
hamlucas123 <Hamish.Lucas1@student.lshtm.ac.uk>
2022-01-19 15:31:04 +0000

    Update worldbankCLEAN.R
    - script to filter for most recent date (country_predict.R)
    - select columns

--- worldbankCLEAN.R
+++ worldbankCLEAN.R
@@ -1,10 +1,26 @@
 # World Bank Data Cleaning
 
+library(dplyr)
+library(tidyr)
+
 df <- read.csv('data/country-level-predictors.csv')
 
 # replacing all ".." with NA
-# df[df==".."] <- NA
+df[df==".."] <- NA
 
 # create column with last known value for each predictor
 df$lastval <- df[cbind( 1:nrow(df), max.col(!is.na(df),"last") )]
-df$lastval <- as.numeric(df$lastval)
+
+# If lastval = Series.Code, change to NA
+df$value <- ifelse(df$Series.Code == df$lastval, NA, df$lastval)
+
+# rename first column
+colnames(df)[1] <- "Country.Name"
+
+# select base columns
+df <- df[c("Country.Name","Country.Code","Series.Name", "value")]
+
+
+
+
+# df$lastval <- as.numeric(df$lastval)

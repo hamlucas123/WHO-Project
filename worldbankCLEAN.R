664fcfd8ae32aea0de2abd54dd5dae406cb32e7a
doctor-code1 <78969505+doctor-code1@users.noreply.github.com>
2022-01-20 11:53:39 +0000

    Update worldbankCLEAN.R
    Reviewed each predictor/changed each variable name/checked for number of missing data

--- worldbankCLEAN.R
+++ worldbankCLEAN.R
@@ -48,7 +48,7 @@ df <- df[c("Country.Name", "Country.Code", "GDP per capita (current US$)",
            "Hospital beds (per 1,000 people)",
            "Refugee population by country or territory of asylum")]
 
-#review column names
+#Review column names
 colnames(df)
 
 #Change predictor names
@@ -58,31 +58,59 @@ names(df)[4] <- 'Health expenditure per capita (current US $)'
 names(df)[5] <- 'Poverty head count ratio at $1.90 a day (% of population)'
 names(df)[6] <- 'Poverty head count ratio at $3.20 a day (% of population)'
 names(df)[7] <- "Poverty headcount ratio at $5.50 a day (% of population)" 
-names(df)[10] <- 'Adult Literacy rate (% of people ages 15 and above)" '
-#Need to review column 11 ??how is it school enrolment and Gender parity
+names(df)[10] <- 'Adult Literacy rate (% of people ages 15 and above)'
+names(df)[11] <- 'Gender Parity Index (GPI)'
 names(df)[12] <- "Education, at least completed primary, population 25+ years, total (%) (cumulative)" 
 names(df)[13] <- 'Education, at least completed upper secondary, population 25+, total (%) (cumulative)'
 names(df)[14] <- "Education, at least Bachelor's or equivalent, population 25+, total (%) (cumulative)"
 
-#Check missing data for each column
-sum(is.na(df[2]))
-sum(is.na(df[3]))
-sum(is.na(df[4]))
+#Check value ranges/missing data
 
-sum(is.na(df[4]))
+summary(as.numeric(unlist(df[,3])))
+#no missing data for GDP per capita, numbers look ok
+
+summary(as.numeric(unlist(df[,4])))
 which(is.na(df[4]))
 #two missing values for health expenditure per capita - Kosovo & Liechtenstein
-#Kosovo public expenditure on health was 3.5 % of GDP in 2019 - can't find data on per capita
-#https://ec.europa.eu/eurostat/statistics-explained/index.php?title=Enlargement_countries_-_health_statistics
-#https://www.nationmaster.com/nmx/timeseries/liechtenstein-healthcare-expenditure-on-healthcare-providers
-#Liechenstein 9136 euros per capita in 2019
 
-sum(is.na(df[5]))
+summary(as.numeric(unlist(df[,5])))
+summary(as.numeric(unlist(df[,6])))
+summary(as.numeric(unlist(df[,7])))
 which(is.na(df[5]))
-sum(is.na(df[6]))
 which(is.na(df[6]))
-sum(is.na(df[7]))
 which(is.na(df[7]))
-#same countries missing data for all 3 measurements of poverty head count ratio
 df[c(2,30,34,43,51),]
-#Andorra, Liechtenstein, Monaco, San Marino, Turkmenistan
+#same countries missing data for all 3 measurements of poverty head count ratio - #Andorra, Liechtenstein, Monaco, San Marino, Turkmenistan
+
+summary(as.numeric(unlist(df[,8])))
+#5 missing for Gini index
+
+summary(as.numeric(unlist(df[,9])))
+#44 indicators missing for CPIA gender inequality rating.. may need to exclude this or find a similar alternative
+
+summary(as.numeric(unlist(df[,10])))
+#22 countries missing data for literacy rate.. same as above
+
+summary(as.numeric(unlist(df[,11])))
+#4 missing for Gender parity index
+
+summary(as.numeric(unlist(df[,12])))
+#10 missing for school enrolment - primary
+
+summary(as.numeric(unlist(df[,13])))
+#4 missing for school enrolment - secondary
+
+summary(as.numeric(unlist(df[,14])))
+#9 missing for school enrolment - bachelors
+
+summary(as.numeric(unlist(df[,15])))
+#2 missing nurses/1,000
+
+summary(as.numeric(unlist(df[,16])))
+#2 missing physicians/1,000
+
+summary(as.numeric(unlist(df[,17])))
+#2 missing hospital beds per 1,000
+
+summary(as.numeric(unlist(df[,18])))
+#3 missing for refugee population

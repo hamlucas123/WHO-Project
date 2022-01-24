02625bffb0951f058255e17de81a7a9619ec6040
Ronnie NG <Ronnie@Ronnies-MacBook-Pro-2.local>
2022-01-24 22:41:21 +0000

    code error resolved

--- euro_count_dict.R
+++ euro_count_dict.R
@@ -1,3 +1,6 @@
+library(xlsx)
+library(dplyr)
+library(tidyr)
 # country namnes from WHO
 euro_count<-c("Albania", "Andorra", "Armenia", "Austria", "Azerbaijan",
               "Belarus", "Belgium", "Bosnia and Herzegovina","Bulgaria",
@@ -20,15 +23,15 @@ names(euro_count_dict) <- eu_heads
 euro_count_dict$Country <- sort(euro_count)
 
 # code used to find missing countries 
-euro_count[!(euro_count %in% iso2_code$Definition)]
+#euro_count[!(euro_count %in% iso2_code$Definition)]
 
 # for iso3 codes
-iso3_code <- read.xlsx("~/LSHTM_DC/CLASS.xls", 1)
+iso3_code <- read.xlsx("~/WHO-Project/data/CLASS.xls", 1)
 iso3_missing <- iso3_code %>% #some names don't matched, need to specifically filter 
   filter( NA..1 %in% c("Czech Republic","Kyrgyz Republic", "Moldova", "Slovak Republic", "United Kingdom"))
 
 # for iso2 codes
-iso2_code <- read.xlsx("~/LSHTM_DC/iso_2digit_alpha_country_codes.xls",1)
+iso2_code <- read.xlsx("~/WHO-Project/data/iso_2digit_alpha_country_codes.xls",1)
 colnames(iso2_code) <- iso2_code[1,]
 iso2_code <- tail(iso2_code,-1)
 iso2_missing <- iso2_code %>%
@@ -43,6 +46,10 @@ euiso3_code <- iso3_code %>%
   arrange(NA..1)
 names(euiso3_code) <- c("Country", "ISO3")
 
+#if iso_code3 country names don't match with WHO, paste in new column
+euro_count_dict$`Other Name Variations` <-ifelse(!(euro_count_dict$Country==euiso3_code$Country), 
+                                                 paste(euiso3_code$Country), NA)
+
 euiso2_code <- iso2_code %>%
   filter(Definition %in% euro_count_dict$Country | Definition %in% euro_count_dict$`Other Name Variations`) %>%
   bind_rows(iso2_missing) %>%
@@ -54,10 +61,8 @@ euiso2_code <- euiso2_code[c(1:29,31:35,30,36:38,40:42,39,43:nrow(euiso2_code)),
 #putting data into euro_count_dict
 euro_count_dict$ISO3 <- euiso3_code$ISO3
 euro_count_dict$ISO2 <- euiso2_code$`Code Value`
-euro_count_dict$`Other Name Variations` <-ifelse(!(euro_count_dict$Country==euiso3_code$Country), 
-                                                 paste(euiso3_code$Country), NA)
-euro_count_dict$`Other Name Variations1` <-ifelse(!(euro_count_dict$Country==euiso2_code$Definition)&
-                                                    !(euro_count_dict$`Other Name Variations`==euiso2_code$Definition), 
+
+euro_count_dict$`Other Name Variations1` <-ifelse(!(euro_count_dict$Country==euiso2_code$Definition), 
                                                   paste(euiso2_code$Definition), NA)
 
 save(euro_count_dict, file=("data/euro_count_dict.csv")) 

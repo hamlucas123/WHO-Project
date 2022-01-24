3d427133bf7f8326214202c3078e05051e3ed272
Ronnie NG <Ronnie@Ronnies-MacBook-Pro-2.local>
2022-01-24 23:12:56 +0000

    using euro count dict

--- vax_fluCLEAN.R
+++ vax_fluCLEAN.R
@@ -1,5 +1,8 @@
-dataflu <- read.csv("~/LSHTM_DC/DP_LIVE_21012022160052490.csv")
+dataflu <- read.csv("data/OWD_flu65.csv")
+euro_count <- read.csv("data/euro_count_dict.csv")
 # 23 countries included
+
 dataflu <- dataflu %>%
-  filter(LOCATION %in% euro_count_dict$`Country Code (WorldBank)`) %>%
-  filter(TIME == "2018")
+  filter(LOCATION %in% euro_count$ISO3) %>%
+  filter(TIME == "2018") %>%
+  select(c("LOCATION", "TIME", "Value"))

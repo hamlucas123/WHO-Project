cbc1777de052ad8a3b3a3000406e0dcf5916a450
Ronnie NG <Ronnie@Ronnies-MacBook-Pro-2.local>
2022-01-24 22:54:05 +0000

    using euro_count_dict to find matching names

--- eth_fracCLEAN.R
+++ eth_fracCLEAN.R
@@ -1,23 +1,16 @@
-data <- read.csv("~/LSHTM_DC/ethnicfrac.csv")
+data <- read.csv("data/ethnicfrac.csv")
+euro_count <- read.csv("data/euro_count_dict.csv")
 # data from https://worldpopulationreview.com/country-rankings/most-racially-diverse-countries
 # 51 countries included 
 # missing Montenegro, North Macedonia
 
-euro_count<-c("Albania", "Andorra", "Armenia", "Austria", "Azerbaijan",
-                "Belarus", "Belgium", "Bosnia and Herzegovina","Bulgaria",
-                "Croatia", "Cyprus", "Czechia", 
-                "Denmark", "Estonia", "Finland", "France", "Georgia", "Germany",
-                "Greece", "Hungary", "Iceland", "Ireland", "Israel", "Italy",
-                "Kazakhstan", "Kyrgyzstan", "Latvia", "Lithuania", "Luxembourg",
-                "Malta", "Monaco", "Montenegro", "Netherlands", "North Macedonia",
-                "Norway", "Poland", "Portugal", "Republic of Moldova", "Romania", 
-                "Russian Federation", "San Marino", "Serbia", "Slovakia", "Slovenia",
-                "Spain", "Sweden", "Switzerland", "Tajikistan", "Turkey", "Turkmenistan",
-                "Ukraine", "United Kingdom of Great Britain and Northern Ireland", "Uzbekistan")
 
-
-
+eth_missing <- data %>%
+  filter(country == "Russia")
 eth_frac <- data %>%
-  filter(country %in% euro_count) 
+  filter(country %in% euro_count$Country | country %in% euro_count$Other.Name.Variations 
+         | country %in% euro_count$Other.Name.Variations1) %>%
+  rbind(eth_missing) %>%
+  arrange(country)
 
 euro_count[!(euro_count %in% eth_frac$country)]

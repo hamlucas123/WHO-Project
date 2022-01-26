ae91eb688ed3f4caf89f8e192e4a81f6e6a8bcd6
hamlucas123 <Hamish.Lucas1@student.lshtm.ac.uk>
2022-01-26 15:15:40 +0000

    Update vax_fluCLEAN.R

--- vax_fluCLEAN.R
+++ vax_fluCLEAN.R
@@ -2,7 +2,14 @@ dataflu <- read.csv("data/OWD_flu65.csv")
 euro_count <- read.csv("data/euro_count_dict.csv")
 # 23 countries included
 
+colnames(dataflu)[1] <- 'LOCATION'
+
 dataflu <- dataflu %>%
   filter(LOCATION %in% euro_count$ISO3) %>%
   filter(TIME == "2018") %>%
   select(c("LOCATION", "TIME", "Value"))
+
+rm(euro_count)
+
+vax_flu <- dataflu
+rm(dataflu)

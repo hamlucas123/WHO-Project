d888755accac5df80df4213c0c2485d25ea91877
hshwong <hadrian.wong1@student.lshtm.ac.uk>
2022-01-28 14:15:24 +0000

    updated to have annotation

--- vaccineEDA.R
+++ vaccineEDA.R
@@ -18,6 +18,14 @@ summarise(df,
 sum(na.omit(df$vax_rate) < 70)
 sum(na.omit(df$vax_rate) > 70)
 
+df %>%
+  select(Country.x, vax_rate) %>%
+  filter(vax_rate == max(na.omit(vax_rate)))
+
+df %>%
+  select(Country.x, vax_rate) %>%
+  filter(vax_rate == min(na.omit(vax_rate)))
+
 vax_pop_plot <- ggplot(data = df[complete.cases(df$vax_rate),],
                        aes(x = reorder(`Country Code`, vax_rate),
                            y = vax_rate,
@@ -29,7 +37,8 @@ vax_pop_plot <- ggplot(data = df[complete.cases(df$vax_rate),],
   ylim(0, 100)+
   scale_size_continuous(breaks = c(25,50,75,100,125),
                         range = c(1,5)) +
-  geom_hline(yintercept=70, linetype="dashed", color = "red")
+  geom_hline(yintercept=70, linetype="dashed", color = "red")+
+  annotate('text', y=72, x= 'UKR', label ='70% vaccination rate', size = 3)
 
 vax_pop_plot
 
@@ -48,7 +57,8 @@ vax_plot <- ggplot(data = df[complete.cases(df$vax_rate),],
   labs(x = 'Country', y = 'Vaccination rate by total population (%)', title = 'COVID-19 Fully vaccinated rate of WHO Europe countries')+
   scale_x_discrete(guide = guide_axis(n.dodge=3)) +
   ylim(0, 100)+
-  geom_hline(yintercept=70, linetype="dashed", color = "red")
+  geom_hline(yintercept=70, linetype="dashed", color = "red")+
+  annotate('text', y=72, x= 'UKR', label ='70% vaccination rate', size = 3)
 
 vax_plot
 

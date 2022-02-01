823aa0b27540a8923af5e4cf5acb09d78616baee
Ronnie NG <Ronnie@Ronnies-MacBook-Pro-2.local>
2022-02-01 15:54:16 +0000

    summary statistics found, need to create summary table

--- predictEDA.R
+++ predictEDA.R
@@ -9,15 +9,31 @@ tot_dat <- tot_dat %>%
 # find cut off for 25th and 85th percentile
 split_dat <- quantile(tot_dat$vax_rate, probs = c(0.25, 0.85), na.rm = TRUE)
 
+# function to find summary statistics
+sumfun <- function(x) {
+  n <- sum(!is.na(x))
+  m <- mean(na.omit(x))
+  s <- sd(na.omit(x))
+  se <- s/sqrt(n)
+  c(N=n, mean=m, sd = s, se = se, lwr=m-1.96*se, upr=m+1.96*se)
+}
+
 # for the 25th percentile
 low_q <- tot_dat %>%
   filter(vax_rate <= split_dat[[1]])
 
 low_qN <- low_q[,sapply(low_q, is.numeric)]
-low_se <- apply(low_qN, 2, sd, na.rm=TRUE)
-low_mean <- apply(low_qN, 2, mean, na.rm=TRUE)
-low_sum <- as.data.frame(c(low_se, low_mean), row.names = names(low_q))
+
+
+
+lowQ1 <- apply(low_qN, MARGIN = 2, FUN = sumfun)
+
 
 # for 85th percentile
 upp_q <- tot_dat %>%
   filter(vax_rate >= split_dat[[2]])
+
+upp_qN <- upp_q[,sapply(upp_q, is.numeric)]
+uppQ1 <- apply(upp_qN, MARGIN = 2, FUN = sumfun)
+
+

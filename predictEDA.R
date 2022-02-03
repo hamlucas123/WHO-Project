48266a07aff7cf92b04b4589e76189251f6e7c46
Ronnie NG <Ronnie@Ronnies-MacBook-Pro-2.local>
2022-02-03 18:03:57 +0000

    html table created -need to rename rows

--- predictEDA.R
+++ predictEDA.R
@@ -1,12 +1,14 @@
 #install.packages("expss")
-#install.packages("fabricatr")
+install.packages("qwraps2")
 library(htmlTable)
 library(magrittr)
 library(dplyr)
 library(ggplot2)
 library(expss)
+library(gtsummary)
 library(tidyr)
-library(fabricatr)
+library(qwraps2)
+
 
 
 setwd ("~/WHO-Project")
@@ -26,6 +28,14 @@ sumfun <- function(x) {
   c(N=n, mean=m, sd = s)
 }
 
+# function to categorise data
+cat_dat <- function(x) {
+  ifelse(x$pred %in% healthsys, 1, 
+         ifelse(x$pred %in% gen_se, 2,
+                ifelse(x$pred %in% soc, 3,
+                       ifelse(x$pred %in% gov, 4,
+                              0))))
+}
 # table of predictors and categories
 healthsys <- c("Hospital.beds..per.1.000.people.", "Health.expenditure.per.capita..current.US...", 
                "Physicians..per.1.000.people." )
@@ -48,11 +58,7 @@ datQ1_tab <- t(as.data.frame(apply(datQ1_n, MARGIN = 2, FUN = sumfun)))
 datQ1_tab<- cbind(pred = rownames(datQ1_tab), data.frame(datQ1_tab, row.names = NULL))
 
 #categorising predictors
-datQ1_tab$cat <- ifelse(datQ1_tab$pred %in% healthsys, 1, 
-                      ifelse(datQ1_tab$pred %in% gen_se, 2,
-                             ifelse(datQ1_tab$pred %in% soc, 3,
-                                    ifelse(datQ1_tab$pred %in% gov, 4,
-                                           0))))
+datQ1_tab$cat <- cat_dat(datQ1_tab)
 datQ1_tab <- datQ1_tab %>%
   filter(!(cat==0))
 
@@ -66,11 +72,7 @@ datQ2_tab <- t(as.data.frame(apply(datQ2_n, MARGIN = 2, FUN = sumfun)))
 datQ2_tab<- cbind(pred = rownames(datQ2_tab), data.frame(datQ2_tab, row.names = NULL))
 
 #categorising predictors
-datQ2_tab$cat <- ifelse(datQ2_tab$pred %in% healthsys, 1, 
-                        ifelse(datQ2_tab$pred %in% gen_se, 2,
-                               ifelse(datQ2_tab$pred %in% soc, 3,
-                                      ifelse(datQ2_tab$pred %in% gov, 4,
-                                             0))))
+datQ2_tab$cat <- cat_dat(datQ2_tab)
 datQ2_tab <- datQ2_tab %>%
   filter(!(cat==0))
 
@@ -84,11 +86,7 @@ datQ3_tab <- t(as.data.frame(apply(datQ3_n, MARGIN = 2, FUN = sumfun)))
 datQ3_tab<- cbind(pred = rownames(datQ3_tab), data.frame(datQ3_tab, row.names = NULL))
 
 #categorising predictors
-datQ3_tab$cat <- ifelse(datQ3_tab$pred %in% healthsys, 1, 
-                        ifelse(datQ3_tab$pred %in% gen_se, 2,
-                               ifelse(datQ3_tab$pred %in% soc, 3,
-                                      ifelse(datQ3_tab$pred %in% gov, 4,
-                                             0))))
+datQ3_tab$cat <- cat_dat(datQ3_tab)
 datQ3_tab <- datQ3_tab %>%
   filter(!(cat==0))
 
@@ -102,11 +100,7 @@ datQ4_tab <- t(as.data.frame(apply(datQ4_n, MARGIN = 2, FUN = sumfun)))
 datQ4_tab<- cbind(pred = rownames(datQ4_tab), data.frame(datQ4_tab, row.names = NULL))
 
 #categorising predictors
-datQ4_tab$cat <- ifelse(datQ4_tab$pred %in% healthsys, 1, 
-                        ifelse(datQ4_tab$pred %in% gen_se, 2,
-                               ifelse(datQ4_tab$pred %in% soc, 3,
-                                      ifelse(datQ4_tab$pred %in% gov, 4,
-                                             0))))
+datQ4_tab$cat <- cat_dat(datQ4_tab)
 datQ4_tab <- datQ4_tab %>%
   filter(!(cat==0))
 
@@ -116,4 +110,50 @@ datQ4_tab <- datQ4_tab %>%
 Q_tab <- inner_join(datQ1_tab[1:4], datQ2_tab[1:4], by = "pred", suffix = c(".Q1", ".Q2"))
 Q_tab1 <-inner_join(datQ3_tab[1:4], datQ4_tab[1:4], by = "pred", suffix = c(".Q3",".Q4"))
 Q_FIN <- inner_join(Q_tab, Q_tab1, by = "pred")
+Q_FIN$cat <- cat_dat(Q_FIN)
+Q_FIN$N_tot <- rowSums(Q_FIN[,c("N.Q1", "N.Q2", "N.Q3", "N.Q4")])
+Q_FIN <- Q_FIN %>%
+  arrange(cat) %>%
+  select(c("pred","N_tot", "mean.Q1","sd.Q1",
+           "mean.Q2","sd.Q2","mean.Q3","sd.Q3",
+           "mean.Q4","sd.Q4"))
+rownames(Q_FIN) <- 
+htmlTable(Q_FIN,
+          header = c("Country-level Predictors",
+            "Total Number", "Mean", 
+            "Std", "Mean", "Std","Mean", 
+            "Std", "Mean", "Std"),
+          rgroup = c("Health System", "General Socioeconomic Status",
+                     "Social/Cultural","Governance"),
+          n.rgroup = c(3,4,5,3),
+          cgroup = c("","< 25th Percentile", "25th-50th Percentile", 
+                     "50th-75th Percentile", "> 75th Percentile"),
+          n.cgroup = c(2,2,2,2,2))
 
+
+
+
+summary_QFIN <- 
+  list("Health System" = 
+         list("Health Expenditure per Capita" = Q_FIN["Health.expenditure.per.capita..current.US...",],
+              "Hospital Beds per 1,000 People" = Q_FIN["Hospital.beds..per.1.000.people.",],
+              "Physicians per 1,000 People" = Q_FIN["Physicians..per.1.000.people.",]),
+       "General Socioeconomic Status" =
+         list("GDP per Capital" = Q_FIN["GDP.per.capita..current.US..", ],
+              "Poverty Headcount Ratio at 3.2 a day" = Q_FIN["Poverty.head.count.ratio.at..3.20.a.day....of.population.",],
+              "Poverty Headcount Ratio at 5.5 a day" = Q_FIN["Poverty.headcount.ratio.at..5.50.a.day....of.population.",],
+              "Education (Secondary)" = Q_FIN["Education..at.least.completed.upper.secondary..population.25...total......cumulative.",]),
+       "Social/Cultural" = 
+         list("Gender Inequality Index" = Q_FIN["Gender.Inequality.Index..GII.",],
+              "Linguistic Fractionalization" = Q_FIN["linguisticFractionalization",],
+              "Ethnic Fractionalization" = Q_FIN["ethnicFractionalization",],
+              "Religious Fractionalization" = Q_FIN["religiousFractionalization", ],
+              "Refugee Population" = Q_FIN["Refugee.population.by.country.or.territory.of.asylum",]),
+       "Governance" = 
+         list("Government Stringency Index" = Q_FIN["stringency_index", ],
+              "Government Effectiveness" = Q_FIN["Government.Effectiveness.Index.Rank", ],
+              "Transparency and Correuption Index" = Q_FIN["CPI.score.2020..Corruption.Index.",]))
+sum_COM <- summary_table(Q_FIN, summaries = summary_QFIN, by = c("pred"))
+                         
+                         
+                         

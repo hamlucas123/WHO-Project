86059822ecda025df815f79c433fe5b269d904cc
hshwong <hadrian.wong1@student.lshtm.ac.uk>
2022-02-04 00:12:48 +0000

    updated plots and vulnerable plot

--- vaccineEDA.R
+++ vaccineEDA.R
@@ -15,9 +15,14 @@ library(dplyr)
 library(lemon)
 library(kableExtra)
 
+euro_count <- read.csv('data/euro_count_dict.csv')
+
 df <- df %>% mutate(vax_rate = coalesce(percent_fully_vaccinated_total_pop*100, people_fully_vaccinated_per_hundred))
 
 df <- df[!df$Country.x == 'Liechtenstein',]
+
+df <- df %>% mutate(x = rep(1, nrow(df)))
+df <- df %>% mutate(y = rep(1, nrow(df)))
 
 summarise(df,
           Mean = mean(na.omit(vax_rate)),
@@ -34,6 +39,58 @@ df %>%
 df %>%
   select(Country.x, vax_rate) %>%
   filter(vax_rate == min(na.omit(vax_rate)))
+
+table <- df %>% 
+  select('Country' = Country.x, 
+         'Code' = `Country Code`,
+         'Vaccination rate' = vax_rate,
+         'Total Population' = population)
+
+table %>%
+  kbl(caption = 'Vaccination rate and Population of WHO Europe Countries') %>%
+  kable_classic() %>%
+  kable_styling() %>%
+  save_kable(file = "plots/table.pdf", self_contained = T)
+
+
+mygrid <- data.frame(
+  row = c(1, 1, 1, 1, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8),
+  col = c(1, 5, 6, 7, 8, 1, 2, 5, 4, 8, 9, 8, 5, 7, 4, 6, 10, 11, 12, 9, 10, 11, 12, 6, 2, 3, 5, 4, 7, 8, 12, 10, 11, 3, 9, 6, 5, 2, 1, 4, 8, 7, 7, 8, 6, 4, 5, 9, 7, 8, 3, 10, 11),
+  code = c("IS", "NO", "SE", "FI", "EE", "IE", "GB", "DK", "NL", "LV", "BY", "LT", "DE", "PL", "BE", "CZ", "RU", "KZ", "KG", "UA", "GE", "UZ", "TJ", "SK", "AD", "FR", "AT", "LU", "RS", "RO", "TM", "AM", "AZ", "MC", "MD", "HU", "SI", "ES", "PT", "CH", "BG", "BA", "ME", "MK", "HR", "IT", "SM", "TR", "AL", "GR", "MT", "CY", "IL"),
+  name = c("Iceland", "Norway", "Sweden", "Finland", "Estonia", "Ireland", "United Kingdom", "Denmark", "Netherlands", "Latvia", "Belarus", "Lithuania", "Germany", "Poland", "Belgium", "Czechia", "Russian Federation", "Kazakhstan", "Kyrgyz Republic", "Ukraine", "Georgia", "Uzbekistan", "Tajikistan", "Slovakia", "Andorra", "France", "Austria", "Luxembourg", "Serbia", "Romania", "Turkmenistan", "Armenia", "Azerbaijan", "Monaco", "Moldova", "Hungary", "Slovenia", "Spain", "Portugal", "Switzerland", "Bulgaria", "Bosnia and Herzegovina", "Montenegro", "Macedonia", "Croatia", "Italy", "San Marino", "Turkey", "Albania", "Greece", "Malta", "Cyprus", "Israel"),
+  stringsAsFactors = FALSE
+)
+
+euro_count <- euro_count[match(mygrid$code, euro_count$ISO2),]
+mygrid$code <- euro_count$ISO3
+
+euro_grid <- ggplot(df, aes(x, y)) +
+  facet_geo(~`Country Code`, grid = mygrid, label = 'code')+
+  geom_point(aes(colour = vax_rate, size = population/1000000 )) +
+  scale_colour_gradient2(low = "red",
+                         mid = 'blue',
+                         high = "blue",
+                         midpoint = 70)+
+  labs(x = '', y = '', title = '',
+       size = 'Population per million', colour = 'Vaccination Rate (%)')+
+  scale_size_continuous(breaks = c(25,50,75,100,125),
+                        range = c(1,5))+
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+
+euro_grid
+
+
+
+
+
+
+#############################end###################################
 
 vax_pop_plot <- ggplot(data = df[complete.cases(df$vax_rate),],
                        aes(x = reorder(`Country Code`, vax_rate),
@@ -89,8 +146,6 @@ count[count == 'North Macedonia'] <- 'Macedonia, the former Yugoslav Republic of
 
 df <- df %>% mutate(country = count)
 
-df <- df %>% mutate(x = rep(1, length(df$country)))
-df <- df %>% mutate(y = rep(1, length(df$country)))
 
 euro_grid_70 <- ggplot(df[complete.cases(df$vax_rate)], aes(x, y)) +
   facet_geo(~country, grid = 'europe_countries_grid1', label = 'code') +
@@ -170,21 +225,5 @@ euro_grid_nopop_70
 
 library(lemon)
 knit_print.data.frame <- lemon_print
-
-table <- df %>% 
-  select('Country' = Country.x, 
-         'Code' = `Country Code`,
-         'Vaccination rate' = vax_rate,
-         'Total Population' = population)
-
-table %>%
-  kbl(caption = 'Vaccination rate and Population of WHO Europe Countries') %>%
-  kable_classic() %>%
-  kable_styling() %>%
-  save_kable(file = "plots/table.pdf", self_contained = T)
-
-euro_count <- read.csv('data/euro_count_dict.csv')
-setdiff(df$Country.x, euro_count$Country)
-setdiff(euro_count$Country, df$Country.x)
 
 

c2b4224f2a72cd109f4bdeb4de7e8aad0d39d318
ngr4 <97607817+ngr4@users.noreply.github.com>
2022-02-10 04:14:13 +0000

    Delete vax_child(mmr_dtp)CLEAN.R

--- CleanData/vax_child(mmr_dtp)CLEAN.R
+++
@@ -1,9 +0,0 @@
-datavax_child <- read.csv("data/OWD_child.csv")
-euro_count_dict <- read.csv("data/euro_count_dict.csv")
-#29 countries included
-colnames(datavax_child)[1] <- 'LOCATION'
-datavax_child1 <- datavax_child %>%
-  filter(LOCATION %in% euro_count_dict$`Country Code (WorldBank)`) %>%
-  filter(TIME == "2018")
-
-length(unique(datavax_child1$LOCATION))

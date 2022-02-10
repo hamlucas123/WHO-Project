6a62401373b633c894d5329572132988edb4df68
hshwong <hadrian.wong1@student.lshtm.ac.uk>
2022-02-10 00:45:36 +0000

    update plots

--- vaccineEDA.R
+++ vaccineEDA.R
@@ -90,32 +90,36 @@ euro_grid <- ggplot(df, aes(x, y)) +
 euro_grid
 
 vul_pop <- df %>%
-  select(`Country.Code`, colnames(df)[c(59,64,69,71)])
+  select(`Country.x`, colnames(df)[c(59,64,69,71)])
 
 vul_pop <- vul_pop[complete.cases(vul_pop),]
 vul_pop <- vul_pop[with(vul_pop, order(-vax_rate)),]
-vul_pop <- vul_pop[,-5]
+vul_pop$vax_rate <- vul_pop$vax_rate/100
 
 vul_pop_long <- pivot_longer(
   data            = vul_pop,
-  cols            = colnames(df)[c(59,64,69)],
+  cols            = colnames(df)[c(59,64,69,71)],
   names_to        = 'group',
-  values_to       = 'vax_rate')
+  values_to       = 'vaccine')
 
 vul_pop_long$group <- factor(vul_pop_long$group, labels = c("Aged above 60 years",
                                                             "Healthcare Worker",
-                                                            'Long Term Foster Care Home'))
+                                                            'Long Term Care Facilities',
+                                                            'General Population'))
 
 
 vul_pop_grid <- ggplot(vul_pop_long)+
-  geom_bar(aes(x = group, y = vax_rate*100, fill = group), position = "stack", stat = "identity")+
-  facet_wrap(~Country.Code)+
+  geom_bar(aes(x = group, y = vaccine*100, fill = group), position = "stack", stat = "identity")+
+  facet_wrap(~Country.x)+
   theme(axis.title.x=element_blank(),
         axis.text.x=element_blank(),
-        axis.ticks.x=element_blank())+
+        axis.ticks.x=element_blank(),
+        panel.background = element_blank(),
+        strip.background =element_rect(fill="black"),
+        strip.text = element_text(colour = 'white'))+
   labs(
     y = 'Vaccination rate (%)',
-    fill = 'Vulnerable group')
+    fill = 'Population Groups')
 
 vul_pop_grid
 
@@ -176,8 +180,14 @@ europeUnionTable <- data.frame(country = df$Country.x, value = df$vax_rate)
 
 euroCoords$value <- europeUnionTable$value[match(euroCoords$region,europeUnionTable$country)]
 
-map_plot <- ggplot() +geom_polygon(data = euroCoords,
-                            aes(x = long, y= lat, group = region, fill = value), colour = "black", size = 0.3)+
+region.lab.data <- euroCoords[,-4] %>%
+  group_by(region) %>%
+  summarise(long = mean(long), lat = mean(lat))
+
+map_plot <- ggplot() +
+  geom_polygon(data = euroCoords,
+               aes(x = long, y= lat, group = region, fill = value),
+               colour = "black", size = 0.3)+
   scale_fill_gradient2(low = "#cc0000",
                        mid = '#3366ff',
                        high = "#3366ff",
@@ -195,7 +205,11 @@ map_plot <- ggplot() +geom_polygon(data = euroCoords,
     plot.margin = unit(0 * c(-1.5, -1.5, -1.5, -1.5), "lines"))+
   coord_map(xlim = c(-20, 80),  ylim = c(32, 71))+
   labs(x = '', y = '', title = '',
-       fill = 'Vaccination Rate (%)')
+       fill = 'Vaccination Rate (%)')+
+  geom_text(aes(x = long, y = lat, label = region),
+            data = region.lab.data,
+            size = 3,
+            hjust = 0.5)
 map_plot
 
 aspect_ratio <- 2.5

1c6edf51342f47b6a4a62b18f8f3d92b52818374
hshwong <hadrian.wong1@student.lshtm.ac.uk>
2022-02-10 01:02:31 +0000

    Update vaccineEDA.R

--- vaccineEDA.R
+++ vaccineEDA.R
@@ -3,7 +3,6 @@
 ## # WHO Data Challenge Project
 ## #############################################################################
 ##
-source('DataMERGE.R')
 
 df <- read.csv('data/merged_data.csv')
 
@@ -14,7 +13,6 @@ df <- read.csv('data/merged_data.csv')
 library(ggplot2)
 library(geofacet)
 library(dplyr)
-library(lemon)
 library(kableExtra)
 library(tidyr)
 
@@ -27,6 +25,8 @@ df <- df[!df$Country.x == 'Kosovo',]
 
 df <- df %>% mutate(x = rep(1, nrow(df)))
 df <- df %>% mutate(y = rep(1, nrow(df)))
+
+## summary statistics ####
 
 summarise(df,
           Mean = mean(na.omit(vax_rate)),
@@ -44,6 +44,8 @@ df %>%
   select(Country.x, vax_rate) %>%
   filter(vax_rate == min(na.omit(vax_rate)))
 
+## Table of vaccinations ####
+
 table <- df %>% 
   select('Country' = Country.x, 
          'Code' = `Country.Code`,
@@ -56,39 +58,7 @@ table %>%
   kable_styling() %>%
   save_kable(file = "plots/table.pdf", self_contained = T)
 
-
-mygrid <- data.frame(
-  row = c(1, 1, 1, 1, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8),
-  col = c(1, 5, 6, 7, 8, 1, 2, 5, 4, 8, 9, 8, 5, 7, 4, 6, 10, 11, 12, 9, 10, 11, 12, 6, 2, 3, 5, 4, 7, 8, 12, 10, 11, 3, 9, 6, 5, 2, 1, 4, 8, 7, 7, 8, 6, 4, 5, 9, 7, 8, 3, 10, 11),
-  code = c("IS", "NO", "SE", "FI", "EE", "IE", "GB", "DK", "NL", "LV", "BY", "LT", "DE", "PL", "BE", "CZ", "RU", "KZ", "KG", "UA", "GE", "UZ", "TJ", "SK", "AD", "FR", "AT", "LU", "RS", "RO", "TM", "AM", "AZ", "MC", "MD", "HU", "SI", "ES", "PT", "CH", "BG", "BA", "ME", "MK", "HR", "IT", "SM", "TR", "AL", "GR", "MT", "CY", "IL"),
-  name = c("Iceland", "Norway", "Sweden", "Finland", "Estonia", "Ireland", "United Kingdom", "Denmark", "Netherlands", "Latvia", "Belarus", "Lithuania", "Germany", "Poland", "Belgium", "Czechia", "Russian Federation", "Kazakhstan", "Kyrgyz Republic", "Ukraine", "Georgia", "Uzbekistan", "Tajikistan", "Slovakia", "Andorra", "France", "Austria", "Luxembourg", "Serbia", "Romania", "Turkmenistan", "Armenia", "Azerbaijan", "Monaco", "Moldova", "Hungary", "Slovenia", "Spain", "Portugal", "Switzerland", "Bulgaria", "Bosnia and Herzegovina", "Montenegro", "Macedonia", "Croatia", "Italy", "San Marino", "Turkey", "Albania", "Greece", "Malta", "Cyprus", "Israel"),
-  stringsAsFactors = FALSE
-)
-
-euro_count <- euro_count[match(mygrid$code, euro_count$ISO2),]
-mygrid$code <- euro_count$ISO3
-
-euro_grid <- ggplot(df, aes(x, y)) +
-  facet_geo(~`Country.Code`, grid = mygrid, label = 'code')+
-  geom_point(aes(colour = vax_rate, size = population/1000000 )) +
-  scale_colour_gradient2(low = "red",
-                         mid = 'blue',
-                         high = "blue",
-                         midpoint = 70)+
-  labs(x = '', y = '', title = '',
-       size = 'Population per million', colour = 'Vaccination Rate (%)')+
-  scale_size_continuous(breaks = c(25,50,75,100,125),
-                        range = c(1,5))+
-  theme(axis.title.x=element_blank(),
-        axis.text.x=element_blank(),
-        axis.ticks.x=element_blank(),
-        axis.text.y=element_blank(),
-        axis.ticks.y=element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank())
-
-euro_grid
-
+## Vulnerable population bar chart ####
 vul_pop <- df %>%
   select(`Country.x`, colnames(df)[c(59,64,69,71)])
 
@@ -123,29 +93,7 @@ vul_pop_grid <- ggplot(vul_pop_long)+
 
 vul_pop_grid
 
-# 
-# vul_pop_long <- pivot_longer(
-#   data            = vul_pop,
-#   cols            = colnames(df)[c(59,64,69)],
-#   names_to        = 'group',
-#   values_to       = 'vax_rate')
-# 
-# vul_pop_grid <- ggplot(vul_pop_long, aes(group, vax_rate*100, fill = group)) +
-#   facet_geo(~`Country Code`, grid = mygrid, label = 'code')+
-#   geom_col()+
-#   labs(x = '', y = '', title = '',
-#        size = 'Population per million', fill = 'Vaccination Rate (%)')+
-#   scale_size_continuous(breaks = c(25,50,75,100,125),
-#                         range = c(1,5))+
-#   theme(axis.title.x=element_blank(),
-#         axis.text.x=element_blank(),
-#         axis.ticks.x=element_blank(),
-#         axis.text.y=element_blank(),
-#         axis.ticks.y=element_blank())
-# 
-# vul_pop_grid
-
-##world map
+##world map ####
 library(grid)
 library(rworldmap)
 worldMap <- getMap()
@@ -190,8 +138,6 @@ region.lab.data <- euroCoords[,-4] %>%
 
 region.lab.data[2:3] <- lapply(region.lab.data[2:3], as.numeric)
 
-
-
 map_plot <- ggplot() +
   geom_polygon(data = euroCoords,
                aes(x = long, y= lat, group = region, fill = value),
@@ -233,141 +179,3 @@ ggsave(
   dpi = 'print',
   height = 5 , width = 8
 )
-
-# #############################end###################################
-# 
-# vax_pop_plot <- ggplot(data = df[complete.cases(df$vax_rate),],
-#                        aes(x = reorder(`Country Code`, vax_rate),
-#                            y = vax_rate,
-#                            size = population/1000000)) + 
-#   geom_point() + 
-#   labs(x = 'Country', y = 'Percentage population fully vaccinated (%)', title = 'COVID-19 Fully vaccinated rate of WHO Europe countries',
-#        size = 'Population per million')+
-#   scale_x_discrete(guide = guide_axis(n.dodge=5)) +
-#   ylim(0, 100)+
-#   scale_size_continuous(breaks = c(25,50,75,100,125),
-#                         range = c(1,5)) +
-#   geom_hline(yintercept=70, linetype="dashed", color = "red")+
-#   annotate('text', y=72, x= 'UKR', label ='70% vaccination rate', size = 3)
-# 
-# vax_pop_plot
-# 
-# ggsave(
-#   filename ='vax_scatter.png',
-#   device = 'png',
-#   path = 'plots',
-#   dpi = 'print'
-# )
-# 
-# 
-# vax_plot <- ggplot(data = df[complete.cases(df$vax_rate),],
-#                    aes(x = reorder(`Country Code`,vax_rate),
-#                        y = vax_rate)) + 
-#   geom_bar(stat = 'identity') + 
-#   labs(x = 'Country', y = 'Vaccination rate by total population (%)', title = 'COVID-19 Fully vaccinated rate of WHO Europe countries')+
-#   scale_x_discrete(guide = guide_axis(n.dodge=3)) +
-#   ylim(0, 100)+
-#   geom_hline(yintercept=70, linetype="dashed", color = "red")+
-#   annotate('text', y=72, x= 'UKR', label ='70% vaccination rate', size = 3)
-# 
-# vax_plot
-# 
-# ggsave(
-#   filename ='vax_bar.png',
-#   device = 'png',
-#   path = 'plots',
-#   dpi = 'print'
-# )
-# 
-# missing <- c("Andorra","Armenia","Azerbaijan",'Georgia','Israel',"Kazakhstan", "Kyrgyz Republic", "Liechtenstein","Monaco",'North Macedonia','San Marino','Tajikistan','Turkmenistan','Uzbekistan')
-# 
-# count <- df$Country.x
-# 
-# count[count == 'Czech Republic'] <- 'Czechia'
-# count[count =='Moldova'] <- 'Moldova, Republic of'
-# count[count == 'Slovak Republic'] <- 'Slovakia'
-# count[count == 'North Macedonia'] <- 'Macedonia, the former Yugoslav Republic of'
-# 
-# df <- df %>% mutate(country = count)
-# 
-# 
-# euro_grid_70 <- ggplot(df[complete.cases(df$vax_rate)], aes(x, y)) +
-#   facet_geo(~country, grid = 'europe_countries_grid1', label = 'code') +
-#   geom_point(aes(colour = vax_rate, size = population/1000000 )) +
-#   scale_colour_gradient2(low = "red",
-#                          mid = 'blue',
-#                        high = "blue",
-#                        midpoint = 70)+
-#   labs(x = '', y = '', title = '',
-#        size = 'Population per million', colour = 'Vaccination Rate (%)')+
-#   scale_size_continuous(breaks = c(25,50,75,100,125),
-#                         range = c(1,5))+
-#   theme(axis.title.x=element_blank(),
-#         axis.text.x=element_blank(),
-#         axis.ticks.x=element_blank(),
-#         axis.text.y=element_blank(),
-#         axis.ticks.y=element_blank(),
-#         panel.grid.major = element_blank(),
-#         panel.grid.minor = element_blank())
-# 
-# euro_grid_70
-# 
-# ggsave(
-#   filename ='euro_grid_70.png',
-#   device = 'png',
-#   path = 'plots',
-#   dpi = 'print'
-# )
-# 
-# euro_grid <- ggplot(df[complete.cases(df$vax_rate)], aes(x, y)) +
-#   facet_geo(~country, grid = 'europe_countries_grid1', label = 'code') +
-#   geom_point(aes(colour = vax_rate, size = population/1000000 )) +
-#   scale_colour_gradient(low ='white',
-#                         high = 'black')+
-#   labs(x = '', y = '')+
-#   theme(axis.title.x=element_blank(),
-#         axis.text.x=element_blank(),
-#         axis.ticks.x=element_blank(),
-#         axis.text.y=element_blank(),
-#         axis.ticks.y=element_blank(),
-#         panel.grid.major = element_blank(),
-#         panel.grid.minor = element_blank())
-# euro_grid
-# 
-# euro_grid_nopop <- ggplot(df[complete.cases(df$vax_rate)], aes(x, y)) +
-#   facet_geo(~country, grid = 'europe_countries_grid1', label = 'name') +
-#   geom_point(aes(colour = vax_rate), size = 5) +
-#   scale_colour_gradient(low ='grey',
-#                         high = 'black')+
-#   labs(x = '', y = '')+
-#   theme(axis.title.x=element_blank(),
-#         axis.text.x=element_blank(),
-#         axis.ticks.x=element_blank(),
-#         axis.text.y=element_blank(),
-#         axis.ticks.y=element_blank(),
-#         panel.grid.major = element_blank(),
-#         panel.grid.minor = element_blank())
-# 
-# euro_grid_nopop
-# 
-# euro_grid_nopop_70 <- ggplot(df[complete.cases(df$vax_rate)], aes(x, y)) +
-#   facet_geo(~country, grid = 'europe_countries_grid1', label = 'name') +
-#   geom_point(aes(colour = vax_rate), size = 5) +
-#   scale_colour_gradient2(low ='yellow',
-#                          mid = 'black',
-#                         high = 'black',
-#                         midpoint = 70)+
-#   labs(x = '', y = '')+
-#   theme(axis.title.x=element_blank(),
-#         axis.text.x=element_blank(),
-#         axis.ticks.x=element_blank(),
-#         axis.text.y=element_blank(),
-#         axis.ticks.y=element_blank(),
-#         panel.grid.major = element_blank(),
-#         panel.grid.minor = element_blank())
-# euro_grid_nopop_70
-# 
-# library(lemon)
-# knit_print.data.frame <- lemon_print
-# 
-# 
